2c691d8fc81a31e8f37c1dc3035769fb0228c7f0
Aviroop Jana <aviroopjana@gmail.com>
2023-03-21 00:43:45 +0530

    added reusable button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,10 @@
-const Button = ({ classStyles }) => (
+const Button = ({ btnName, classStyles, handleClick }) => (
   <button
     type="button"
-    className="nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}"
+    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
+    onClick={handleClick}
   >
-    Button
+    { btnName }
   </button>
 );
 
