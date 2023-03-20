6c31fc065351243b37f0305fd03036e59dd5bca1
Aviroop Jana <aviroopjana@gmail.com>
2023-03-20 07:46:57 +0530

    added pages

---
+++ pages/my-nft.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyNFT = () => (
+  <div>MyNFT</div>
+);
+
+export default MyNFT;
---
+++ pages/nft-details.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NFTdetails = () => (
+  <div>NFTdetails</div>
+);
+
+export default NFTdetails;
