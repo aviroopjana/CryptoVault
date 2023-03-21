bb8f4a9457c3a39d9fcc27717dc865c1b8f36d01
Aviroop Jana <aviroopjana@gmail.com>
2023-03-21 18:47:37 +0530

    updating changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Banner from '../components/Banner';
+
 const Home = () => (
-  <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+  <div className="flex justify-center sm:pz-4 p-12">
+    <div className="w-full minmd:w-4/5">
+      <Banner
+        name="Discover, Collect, and sell extraordinary NFTs"
+        childStyles="md:text-4xl sm:text-2xl sx=text-xl text-left"
+        parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+      />
+    </div>
   </div>
 );
 
