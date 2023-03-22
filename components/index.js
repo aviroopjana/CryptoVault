425b6236ecdbf5d676be7410848dcfb00e12a9a7
Aviroop Jana <aviroopjana@gmail.com>
2023-03-23 02:26:48 +0530

    updated index.js

--- components/index.js
+++ components/index.js
@@ -4,5 +4,6 @@ import Footer from './Footer';
 import Button from './Button';
 import CreatorCard from './CreatorCard';
 import NFTCard from './NFTCard';
+import Input from './Input';
 
-export { Navbar, Footer, Button, CreatorCard, NFTCard };
+export { Navbar, Footer, Button, CreatorCard, NFTCard, Input };
