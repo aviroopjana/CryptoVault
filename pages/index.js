faddc82fd88eaefee1b9122d39dbf679c4cc0629
Aviroop Jana <aviroopjana@gmail.com>
2023-03-23 00:48:06 +0530

    updating index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useTheme } from 'next-themes';
 
 import Banner from '../components/Banner';
 import CreatorCard from '../components/CreatorCard';
+import NFTCard from '../components/NFTCard';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
 
@@ -41,7 +42,7 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          name="Discover, collect, and sell extraordinary NFTs"
+          name="Discover, collect, and sell exceptional NFTs"
           parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
         />
@@ -69,6 +70,27 @@ const Home = () => {
               </>
               )}
             </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <div className="flexBetween mx-4 ms:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot Bids</h1>
+            <div>Search Bar</div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Cool NFT on Sale',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
