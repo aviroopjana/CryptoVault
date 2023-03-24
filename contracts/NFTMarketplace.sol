9c6bd371cd6b7fd7220a37d0a95354ac541518c1
Aviroop Jana <aviroopjana@gmail.com>
2023-03-24 19:59:11 +0530

    Created NFTMarketplace.sol

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTmarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private itemsold;
+
+    uint256 listingPrice = 0.025 ether;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event MarketItemCreated (
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function updateListingPrice (uint _listingPrice) public payable {
+        require(owner == msg.sender, "Only Marketplace owner can update the listing price");
+
+        listingPrice = _listingPrice;
+    }
+
+    function getListingPrice () public view returns (uint256) {
+        return listingPrice;
+    }
+}
